569f8c94ee5f45673ac809368065fd9a2d7d5a30
clinzyA4 <clinzy1@protonmail.com>
2026-06-26 10:45:35 -0700

    Update start script, readme
    Signed-off-by: clinzyA4 <clinzy1@protonmail.com>

--- start.sh
+++ start.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
 
 # Parse args
-if [ "$1" = "--expose" ]; then
-  BIND="0.0.0.0"
-  IP=$(hostname -I | awk '{print $1}')
-else
-  BIND="127.0.0.1"
-  IP="localhost"
-fi
+BIND="127.0.0.1"
+IP="localhost"
+VAULT_PATH="/mnt/vault"
+
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --expose)
+      BIND="0.0.0.0"
+      IP=$(hostname -I | awk '{print $1}')
+      shift
+      ;;
+    --path)
+      if [ -n "$2" ]; then
+        VAULT_PATH="$2"
+        shift 2
+      else
+        echo "Error: --path requires a value"
+        exit 1
+      fi
+      ;;
+    *)
+      shift
+      ;;
+  esac
+done
 
 # Function to cleanup all started background processes
 cleanup() {
@@ -17,6 +35,7 @@ cleanup() {
   [ -n "$CALIBRE_PID" ] && kill "$CALIBRE_PID"
   [ -n "$MEILI_PID" ] && kill "$MEILI_PID"
   [ -n "$HTTP_PID" ] && kill "$HTTP_PID"
+  [ -n "$JELLYFIN_PID" ] && kill "$JELLYFIN_PID"
   wait
   echo "All services stopped."
   exit 0
@@ -26,19 +45,22 @@ trap cleanup INT
 sleep 3
 
 # Start Kiwix
-kiwix-serve --port 8888 --address $BIND /mnt/vault/zim/*.zim &
+kiwix-serve --port 8888 --address $BIND $VAULT_PATH/zim/*.zim &
 KIWIX_PID=$!
 
 # Start Calibre-Web
-/mnt/vault/pdf/calibre-web-env/bin/cps -i $BIND &
+./venv/bin/cps -i $BIND &
 CALIBRE_PID=$!
 
 # Start MeiliSearch
-~/Documents/prep/meilisearch --db-path /mnt/vault/meili-data --http-addr $BIND:7700 --no-analytics &
+./meilisearch --db-path $VAULT_PATH/meili-data --http-addr $BIND:7700 --no-analytics &
 
+# Start Jellyfin
+jellyfin &
+JELLYFIN_PID=$!
 
 # Start landing page server
-python3 -m http.server 5500 --bind $BIND --directory $HOME/Documents/prep &
+python3 -m http.server 5500 --bind $BIND --directory ./ &
 HTTP_PID=$!
 
 echo "All services started:"
@@ -46,6 +68,7 @@ echo "  Landing page: http://$IP:5500"
 echo "  Kiwix:        http://$IP:8888"
 echo "  Calibre-Web:  http://$IP:8083"
 echo "  MeiliSearch:  http://$IP:7700"
+echo "  Jellyfin:     http://$IP:8096"
 
 # Wait forever until trapped (so ctrl+c works)
 wait
